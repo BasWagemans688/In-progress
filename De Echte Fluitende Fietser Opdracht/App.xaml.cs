c723fc921133e5048f81b690b9f2233c70b988ca
P3sens <PS271084@summacollege.nl>
2026-03-30 14:50:51 +0200

    Add files via upload

---
+++ De Echte Fluitende Fietser Opdracht/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace De_Echte_Fluitende_Fietser_Opdracht
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
